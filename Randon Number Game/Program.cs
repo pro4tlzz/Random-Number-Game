23616d5c4357eabc2089bc8e7eaf713d6b367c05
Pro4TLZZ <superfrillz12@hotmail.co.uk>
2018-08-16 12:09:17 +0100

    Added while loop to keep console open

--- Randon Number Game/Program.cs
+++ Randon Number Game/Program.cs
@@ -10,20 +10,21 @@ namespace Randon_Number_Game
     {
          static void Main(string[] args)
          {
-             // Boolean if user passes
-             Boolean Userpassed;
+            // Boolean if user passes
+            Boolean Userpassed = false;
              // Declares Answer Variable
              int answer = 6;
             // Declares integer for actual integer
             int userinput;
-             // Asks user for a number
+            // Runs loop until user passes
+            while (Userpassed == false)
+            {
+            // Asks user for a number
              Console.WriteLine("Enter a number between 1 and 10");
              // Takes User Input as String
             string useranswer = Console.ReadLine();
             // User string converted to Int
             userinput = Convert.ToInt32(useranswer);
-             // Displays Message
-            Console.ReadLine();
             // Checks if input is equal to answer
             if (userinput == answer)
              {
@@ -38,6 +39,7 @@ namespace Randon_Number_Game
              {
                  // User Fails
                  Console.WriteLine("The actual answer is less than what you entered");
+                Userpassed = false;
              }
             // Checks if input is less than answer
             else
@@ -45,9 +47,12 @@ namespace Randon_Number_Game
                  {
                  // User Fails
                  Console.WriteLine("The actual answer is greater than what you entered");
-                 }
+                Userpassed = false;
+            }
              // Displays Message
              Console.ReadLine();
+            }
+         
 
 
 
